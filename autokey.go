443e66e050dd3f574a860e806ef628971286976c
Allen Luce <aluce@github.con.com>
2016-08-29 18:38:30 +0000

    Add tests, speed things up a bit.
    Have it handle full-shuffled and unicode (non-modifying) alphabets.

--- autokey.go
+++ autokey.go
@@ -1,10 +1,12 @@
 package autokey
 
+import "unicode/utf8"
+
 type AutoKey struct {
-	Key              map[int32]rune
-	betaalph         map[rune]int32
+	Key              map[int]int
+	betaalph         map[rune]int
 	alphabet         []rune
-	keylen, alphalen int32
+	keylen, alphalen int
 }
 
 func NewAutoKey(key, alphabet string) *AutoKey {
@@ -14,39 +16,45 @@ func NewAutoKey(key, alphabet string) *AutoKey {
 }
 
 func (ak *AutoKey) Init(key, alphabet string) {
-	ak.Key = make(map[int32]rune)
-	ak.betaalph = make(map[rune]int32)
-	for i, c := range key {
-		ak.Key[int32(i)] = c
-	}
-	for i, c := range alphabet {
-		ak.betaalph[c] = int32(i)
-	}
+	ak.Key = make(map[int]int)
+	ak.betaalph = make(map[rune]int)
+
+	ak.alphalen = utf8.RuneCountInString(alphabet)
 	ak.alphabet = []rune(alphabet)
-	ak.keylen = int32(len(key))
-	ak.alphalen = int32(len(alphabet))
+	ak.keylen = utf8.RuneCountInString(key)
+
+	i := 0
+	for _, c := range alphabet {
+		ak.betaalph[c] = i
+		i++
+	}
+	i = 0
+	for _, c := range key {
+		ak.Key[i] = ak.betaalph[c]
+		i++
+	}
 }
 
 func (ak *AutoKey) Encode(raw string) string {
 	var res []rune
-	cc := int32(ak.keylen)
-	var last []rune
+	cc := ak.keylen
+	var last []int
 
 	for _, c := range raw {
 		if _, ok := ak.betaalph[c]; !ok {
 			res = append(res, c) // Not in alphabet
 		} else {
-			var k int32
+			var k int
 			if cc > 0 {
 				k = ak.Key[cc]
 			} else {
-				k = int32(ak.betaalph[last[0]])
+				k = last[0]
 				last = last[1:]
 			}
 			cc--
 			ch := ak.alphabet[(ak.betaalph[c]+k)%ak.alphalen]
 			res = append(res, ch)
-			last = append(last, ch)
+			last = append(last, ak.betaalph[ch])
 		}
 	}
 	return string(res)
@@ -54,50 +62,25 @@ func (ak *AutoKey) Encode(raw string) string {
 
 func (ak *AutoKey) Decode(raw string) string {
 	var res []rune
-	cc := int32(ak.keylen)
-	var last []rune
+	cc := ak.keylen
+	var last []int
 
 	for _, c := range raw {
 		if _, ok := ak.betaalph[c]; !ok {
 			res = append(res, c) // Not in alphabet
 		} else {
-			var k int32
+			var k int
 			if cc > 0 {
-				k = ak.Key[cc] % ak.alphalen
+				k = ak.Key[cc]
 			} else {
-				k = int32(ak.betaalph[last[0]])
+				k = last[0]
 				last = last[1:]
 			}
 			cc--
 			ch := ak.alphabet[(ak.betaalph[c]-k+ak.alphalen)%ak.alphalen]
 			res = append(res, ch)
-			last = append(last, c)
+			last = append(last, ak.betaalph[c])
 		}
 	}
 	return string(res)
 }
-
-/*
-Autokey.prototype.decode = function (raw) {
-  var res = ''
-  var cc = this.keylen
-  var last = []
-  var k
-  for (var i = 0, ii = raw.length; i < ii; ++i) {
-    if (this.betaalph[raw[i]] === undefined) {
-      res += raw[i]
-    } else {
-      if (cc-- > 0) {
-        k = this.key[cc] % this.alphalen
-      } else {
-        k = this.betaalph[last.shift()]
-      }
-      res += this.alphabet[(this.betaalph[raw[i]] - k + this.alphalen) % this.alphalen]
-      last.push(raw[i])
-    }
-  }
-  return res
-}
-
-module.exports = Autokey
-*/
